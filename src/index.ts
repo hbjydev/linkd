a97a66ef1ae8e6272c15630a6f0d23d4f965f6a7
Hayden Young <hayden@itshayden.dev>
2020-07-01 00:09:08 +0100

    added todos

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import bodyParser from 'koa-bodyparser';
 import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn } from 'typeorm';
 import { nanoid } from 'nanoid';
 
+// TODO add documentation & split this into its own file
 @Entity()
 class URL extends BaseEntity {
   
@@ -27,6 +28,7 @@ class URL extends BaseEntity {
   @UpdateDateColumn()
   public updatedAt!: Date;
 
+  // TODO investigate typeorm soft delete
   @DeleteDateColumn()
   public deletedAt!: Date;
 
@@ -35,16 +37,20 @@ class URL extends BaseEntity {
 const app = new Koa();
 const port = parseInt(process.env.PORT ?? '3000', 10);
 
+// TODO init typeorm
+
 app.use(bodyParser());
 
 const router = new Router({ prefix: '/' });
 
+// TODO create a more descriptive route
 router.get('/', (ctx) => {
   ctx.body = {
     message: 'Welcome to linkd'
   };
 });
 
+// TODO fix routing
 router.post('/url', async (ctx) => {
   const url = ctx.request.body.url;
   if (!url) {
@@ -70,6 +76,7 @@ router.post('/url', async (ctx) => {
       data: newUrl
     };
   } catch (e) {
+    // TODO add error catching based on type
     const message = 'Whoops! Something went wrong on our end!';
     ctx.status = e.statusCode ?? 500;
     let stack = {};
@@ -82,12 +89,22 @@ router.post('/url', async (ctx) => {
   }
 });
 
+// TODO add redirect based on /:nanoid
+
+// TODO allow deleting existing urls based on a provided owner key
+
+// TODO provide endpoint to see all urls
+
 app
   .use(router.routes())
   .use(router.allowedMethods());
 
 app.use(cors());
 
+// TODO add per-request logging
+
 app.listen(port, () => {
+  // TODO implement a better logging structure
+  //   maybe something like "info => Listening on port *:${port}"
   console.log(`Listening on *:${port}`);
 });
